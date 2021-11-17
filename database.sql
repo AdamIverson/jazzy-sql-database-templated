c68e6efcb2180e2b77eb3e82c6872bb48e6dfb74
Adam Iverson <aciverso@gmail.com>
2021-11-16 19:50:44 -0600

    the stuff works but the dates are heiroglyphs

--- database.sql
+++ database.sql
@@ -3,3 +3,26 @@ CREATE TABLE "artists" (
     "artist_name" varchar(80) not null,
     "year_born" date
 );
+
+CREATE TABLE song (
+    "id" serial PRIMARY KEY,
+    "title" varchar(255) NOT NULL,
+    "length" varchar(10),
+    "released" date
+);
+
+INSERT INTO "artist"
+	("name", "birthdate")
+VALUES 
+	('Ella Fitzgerald', '04-25-1917'),
+	('Dave Brubeck', '12-06-1920'),
+	('Miles Davis', '05-26-1926'),
+	('Esperanza Spalding', '10-08-1984')
+	
+INSERT INTO "song"
+	("title", "length", "released")
+VALUES 
+	('Take Five', '5:24', '09-29-1959'),
+	('So What', '9:22', '1959-08-17'),
+	('Black Gold', '5:17', '2012-02-01')
+	
---
+++ server/routes/song.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+// Require the pg module:
+const pg = require('pg');
+
+// Create a pool object constructor.
+const Pool = pg.Pool;
+
+// Create our pool object using the above constructor:
+const pool = new Pool({
+  database: 'jazzy_sql', // the name of database, This can change!
+  host: 'localhost', // where is your database?
+  port: 5432, // the port for your database, 5432 is default for postgres
+  max: 10, // how many connections (queries) at one time
+  idleTimeoutMillis: 30000 // 30 second to try to connect, otherwise cancel query
+});
+
+// Log to our console when our pool object makes a connection:
+pool.on('connect', () => {
+  console.log('Postgresql connected');
+});
+
+// Log to our console when something makes our pool error out:
+pool.on('error', (error) => {
+  console.log('Error with postgres pool', error)
+});
+
+
+router.get('/', (req, res) => {
+  const sqlText = 'SELECT * FROM song;'
+  pool.query(sqlText)
+    .then((dbRes) => {
+      const songsFromDb = dbRes.rows;
+      res.send(songsFromDb)
+    }).catch((dbErr) => {
+      console.error(dbErr);
+    });
+});
+
+router.post('/', (req, res) => {
+  const newSong = req.body;
+  const sqlText = (`
+    INSERT INTO "song"
+      ("title", "length", "released")
+    VALUES
+      ($1, $2, $3);
+  `)
+  const sqlValues = [
+    newSong.title,
+    newSong.length,
+    newSong.released,
+  ]
+  console.log(sqlText)
+  pool.query(sqlText, sqlValues)
+    .then((dbRes) => {
+      res.sendStatus(201);  // OK, CREATED
+    })
+    .catch((dbErr) => {
+      console.error(dbErr);
+    })
+});
+
+module.exports = router;
